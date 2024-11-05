54093383a75f3fa5fb65004d6e6cfb3dbd633473
databus3301 <felix@rose.ruhr>
2024-11-05 19:09:10 +0100

    form querying

--- server/js/landing.js
+++ server/js/landing.js
@@ -1,5 +1,21 @@
 
-async function join_form() {
-    console.log("Join form");
-    window.location.href='https://votium.social/html/answer.html';
-}
+
+document.getElementById('landingForm').addEventListener('submit',
+function(event) {
+            event.preventDefault();
+
+            // TODO: fetch the data for the form
+            // if in html format → set dom to fetch
+            // if in json format → redirect to answer.html and parse the json
+
+
+            console.log("about to redirect")
+            window.location.href='https://votium.social/html/answer.html';
+            console.log("redirected")
+        }
+);
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM loaded");
+});
+
